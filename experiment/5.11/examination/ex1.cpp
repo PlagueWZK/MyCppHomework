6ceb35cc968b6a7ca7274b47548aa512a439f95f
PlagueWZK <2849996270@qq.com>
2024-05-11 19:54:52 +0800

    Fuck this

--- experiment/5.11/examination/ex1.cpp
+++ experiment/5.11/examination/ex1.cpp
@@ -1,14 +1,53 @@
-//for test of examination
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-class {
+class Time
 
+ {public:
 
-};
+   Time(int,int,int);
 
-int main() {
+   void fun(  int &t )
 
+   {
 
-    return 0;
+      hour=t;
+
+      t= 18;
+
+   }
+
+   int hour;
+
+   int minute;
+
+   int sec;
+
+ }; 
+
+Time::Time(int h,int m,int s)
+
+ {   hour=h;
+
+     minute=m;
+
+     sec=s;
+
+ }
+
+int main()
+
+{int x=15;
+
+Time t1(10,13,56);
+
+t1.fun(x);
+
+cout<<t1.hour<<" ";
+
+cout<<x<<endl;
+
+return 0;
+
 }
