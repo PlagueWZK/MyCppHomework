b41273c7ff68e4f9a4076ce1223b4307124a443b
PlagueWZK <2849996270@qq.com>
2024-05-17 11:43:56 +0800

    FIX

--- experiment/test/test.cpp
+++ experiment/test/test.cpp
@@ -22,6 +22,9 @@ int main() {
     cout<<endl;
     char x = 'a';
     char* a = &x;
-    cout<<a<<endl;
-    cout<<*a;
+    cout<<a<<endl; //输出指针所指向的字符并且继续输出知道遇到\0空字符
+    cout<<*a<<endl; //输出指针a所指向的内容
+    int* y = new int(5);//new关键字返回一个int对象的地址
+    cout<<y<<endl;//输出指针y的地址
+    cout<<*y<<endl;//输出指针y指向的int数据
 }
